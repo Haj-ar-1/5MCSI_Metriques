d137368c3833f22295d1f69d2e87f3a3155e3ff0
Hajar-Graini <116266612+Haj-ar-1@users.noreply.github.com>
2024-09-12 16:27:39 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -36,6 +36,10 @@ def mongraphique():
     return render_template("graphique.html")
 
 
+@app.route("/rapport/")
+def monhistogramme():
+    return render_template("histogramme.html")
+
 if __name__ == "__main__":
   app.run(debug=True)
 
